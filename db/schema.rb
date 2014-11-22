78bdc4c34317d4d23abfff1a508339e30f84a18a
Lut Garcia <lutgardo21@gmail.com>
2014-11-22 08:32:57 -0600

    socket: /var/pgsql_socket # <-- this line

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141118182859) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "calculates", force: true do |t|
     t.integer  "product_id"
     t.integer  "qty"
